0db286efc1c68bc157fb210f309b59c0dc5d4657
ZhangZhen <1290049069@qq.com>
2020-11-24 22:34:36 +0800

    add write and read function

--- demomem-2/demomem.c
+++ demomem-2/demomem.c
@@ -3,6 +3,8 @@
 #include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/device.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h> 
 
 
 #define BUF_SIZE        1024
@@ -10,7 +12,8 @@
 #define MINOR_NUM       0
 
 struct demo_device {
-    char buffer[BUF_SIZE];
+//    char buffer[BUF_SIZE];
+    char *buffer;
     struct cdev cdev;
     struct class *cls;
     struct device *device;
@@ -30,13 +33,49 @@ static int demo_release(struct inode *node , struct file *file)
 }
 static ssize_t demo_read(struct file *file , char __user *buf, size_t size, loff_t *pos)
 {
+    int ret;
+    int read_bytes;
+    char *kbuf = demo_dev.buffer + *pos;
+
+    if(*pos >= BUF_SIZE)
+        return -1;
+
+    if(size > (BUF_SIZE - *pos))
+        read_bytes = BUF_SIZE - *pos;
+    else
+        read_bytes = size;
+
+    ret = copy_to_user(buf,kbuf,read_bytes);
+    if(ret != 0)
+        return -EFAULT;
+    *pos += read_bytes;
+
     printk(KERN_INFO "Enter %s\n",__func__);
-    return 0;
+
+    return read_bytes;
 }
 static ssize_t demo_write(struct file *file, const char __user *buf , size_t size , loff_t *pos)
 {
+    int ret;
+    int write_bytes;
+    char *kbuf = demo_dev.buffer + *pos;
+
+    if(*pos >= BUF_SIZE)
+        return -1;
+
+    if(size > (BUF_SIZE - *pos))
+        write_bytes = BUF_SIZE - *pos;
+    else
+        write_bytes = size;
+
+    ret = copy_from_user(kbuf, buf, write_bytes);
+    if(ret != 0)
+        return -EFAULT;
+    *pos += write_bytes;
+
     printk(KERN_INFO "Enter %s\n",__func__);
-    return 0;
+
+    return write_bytes;
 }
 
 static struct file_operations demo_operation = {   
@@ -51,6 +90,13 @@ static int __init demo_init(void)
     int ret =1 ;
     dev_t dev_no;
     dev_no = MKDEV(MAJOR_NUM,MINOR_NUM);
+
+    demo_dev.buffer = (char *)kmalloc(BUF_SIZE,GFP_KERNEL);
+    if(!demo_dev.buffer){
+        printk(KERN_ERR "malloc %d bytes failed \n",BUF_SIZE);
+        ret = -ENOMEM;
+        goto ERROR_MALLOC;
+    }
     //init for demo char 
     cdev_init(&demo_dev.cdev,&demo_operation);
     //register device number
@@ -71,19 +117,19 @@ static int __init demo_init(void)
     }
 
     //create a demomem class in /sys/class/demo/
-    demo_dev.cls = class_create(THIS_MODULE,"demo");
-    if(IS_ERR(demo_dev.cls)){
-        ret = PTR_ERR(demo_dev.cls);
-        goto ERROR_CLASS_CREATE;
-    }
-
-    //creat a demomem device in /sys/class/demo/demomem
-    demo_dev.device = device_create(demo_dev.cls,NULL,
-            dev_no,NULL,"demomem");
-    if(IS_ERR(demo_dev.device)){
-        ret = PTR_ERR(demo_dev.device);
-        goto ERROR_DEVICE_CREATE;
-    }
+//    demo_dev.cls = class_create(THIS_MODULE,"demo");
+//    if(IS_ERR(demo_dev.cls)){
+//        ret = PTR_ERR(demo_dev.cls);
+//        goto ERROR_CLASS_CREATE;
+//    }
+//
+//    //creat a demomem device in /sys/class/demo/demomem
+//    demo_dev.device = device_create(demo_dev.cls,NULL,
+//            dev_no,NULL,"demomem");
+//    if(IS_ERR(demo_dev.device)){
+//        ret = PTR_ERR(demo_dev.device);
+//        goto ERROR_DEVICE_CREATE;
+//    }
     
     printk(KERN_INFO "Enter %s\n",__func__);
     return 0;
@@ -96,6 +142,9 @@ ERROR_CDEV_ADD:
     unregister_chrdev_region(dev_no,1);
     return ret;
 ERROR_CHARDEV_REGION:
+    kfree(demo_dev.buffer);
+    demo_dev.buffer = NULL;
+ERROR_MALLOC:
     return ret;
 }
 
@@ -103,6 +152,9 @@ static void __exit demo_exit(void)
 {
     cdev_del(&demo_dev.cdev);
     unregister_chrdev_region(MKDEV(MAJOR_NUM,MINOR_NUM),1);
+
+    kfree(demo_dev.buffer);
+    demo_dev.buffer = NULL;
     
     printk(KERN_INFO "Enter %s\n",__func__);
 }
