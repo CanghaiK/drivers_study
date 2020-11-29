d10780f1b539b2540312e59ef0c8d978d9a7fb2a
ZhangZhen <1290049069@qq.com>
2020-11-29 14:08:59 +0800

    add misc dev code

---
+++ demomem-4/.demomem.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/zhangzhen/Downloads/linux-5.9.3/drivers/drivers_study/demomem-4/demomem.mod := { echo  /home/zhangzhen/Downloads/linux-5.9.3/drivers/drivers_study/demomem-4/demomem.o; echo; } > /home/zhangzhen/Downloads/linux-5.9.3/drivers/drivers_study/demomem-4/demomem.mod
