528b7ead4a74bdaa5bb65143989278d946b5b35d
ZhangZhen <1290049069@qq.com>
2020-12-02 21:05:36 +0800

    add demomem-semaphore

---
+++ demomem-semaphore/.demomem.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/zhangzhen/Downloads/linux-5.9.3/drivers/drivers_study/demomem-semaphore/demomem.mod := { echo  /home/zhangzhen/Downloads/linux-5.9.3/drivers/drivers_study/demomem-semaphore/demomem.o; echo; } > /home/zhangzhen/Downloads/linux-5.9.3/drivers/drivers_study/demomem-semaphore/demomem.mod
---
+++ demomem-semaphore/test.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include "demomem.h"
+
+#define DEVICE_PATH                 "/dev/demomem"
+
+
+int main(void)
+{
+    int fd;
+    int ret;
+    int n;
+    int val;
+    char buf[10] = "hello\n";
+
+
+    printf("Open!");
+    fd = open(DEVICE_PATH,O_RDWR);
+
+    if(fd<0){
+        printf("failed to open\n");
+        return -1;
+    }
+    printf("sleep\n") ;
+
+    sleep(10);
+
+
+    printf("close\n");
+    close(fd);
+
+
+    return 0;
+
+}
+    
