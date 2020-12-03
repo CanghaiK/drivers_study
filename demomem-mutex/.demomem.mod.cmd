85677a18d8418fe8bf22fe10cc1f2d81cb6abd34
ZhangZhen <1290049069@qq.com>
2020-12-03 15:55:45 +0800

    create demomem-mutex demo

---
+++ demomem-mutex/.demomem.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/zhangzhen/Downloads/linux-5.9.3/drivers/drivers_study/demomem-mutex/demomem.mod := { echo  /home/zhangzhen/Downloads/linux-5.9.3/drivers/drivers_study/demomem-mutex/demomem.o; echo; } > /home/zhangzhen/Downloads/linux-5.9.3/drivers/drivers_study/demomem-mutex/demomem.mod
